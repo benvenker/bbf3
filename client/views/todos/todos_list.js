867862b4eb90bb58b3b54030ba44be765f08c603
Ben Venker <bvemails@gmail.com>
2013-11-12 10:54:23 -0600

    Wired collection into todosList template.

--- client/views/todos/todos_list.js
+++ client/views/todos/todos_list.js
@@ -1,17 +1,5 @@
-var todosData = [
-	{
-		title: 'Back massage',
-		frequency: 'Weekly'
-	},
-	{
-		title: 'Take out the garbage',
-		frequency: 'Daily'
-	},
-	{
-		title: 'Pay a compliment',
-		frequency: 'Monthly'
-	}
-];
 Template.todosList.helpers({
-	todos: todosData
+	todos: function() {
+		return Todos.find();
+	}
 });
