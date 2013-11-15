14abc3e86d20b52aca6af947445004c3705c3f82
Ben Venker <bvemails@gmail.com>
2013-11-15 13:57:58 -0600

    Display comments on Todo's page.

--- client/views/todos/todo_page.js
+++ client/views/todos/todo_page.js
@@ -1,5 +1,8 @@
 Template.todoPage.helpers({
 	currentTodo: function() {
 		return Todos.findOne(Session.get('currentTodoId'));
+	},
+	comments: function() {
+		return Comments.find({todoId: this._id});
 	}
 });
