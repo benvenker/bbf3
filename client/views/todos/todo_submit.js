c00bb33b96180ca71e3b5ec3175e726012c8dcf7
Ben Venker <bvemails@gmail.com>
2013-11-15 19:05:43 -0600

    Created a form to submit comments.

--- client/views/todos/todo_submit.js
+++ client/views/todos/todo_submit.js
@@ -14,7 +14,7 @@ Template.todoSubmit.events({
       if (error){
         // display the error to the user
         throwError(error.reason);
-        // if the error is that the post already exists, take us there
+        // if the error is that the todo already exists, take us there
         if (error.error === 302)
           Meteor.Router.to('todoPage', error.details)
       } else {
--- collections/comments.js
+++ collections/comments.js
@@ -1,1 +1,20 @@
 Comments = new Meteor.Collection('comments');
+Meteor.methods({
+	comment: function(commentAttributes) {
+		var user = Meteor.user();
+		var todo = Todos.findOne(commentAttributes.todoId);
+		// ensure the user is logged in
+		if (!user)
+			throw new Meteor.Error(401, "You need to login to make comments");
+		if (!commentAttributes.body)
+			throw new Meteor.Error(422, 'Please write some content');
+		if (!commentAttributes.todoId)
+			throw new Meteor.Error(422, 'You must comment on a todo');
+		comment = _.extend(_.pick(commentAttributes, 'todoId', 'body'), {
+			userId: user._id,
+			author: user.username,
+			submitted: new Date().getTime()
+		});
+		return Comments.insert(comment);
+	}
+});
